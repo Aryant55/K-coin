0428806f4df1ca075dec1d50a1f9ced556ab9f70
aryant shukla <aeyantshukla@gmail.com>
2020-11-29 19:28:16 +0530

    Create ecc.py

---
+++ ecc.py
@@ -0,0 +1,138 @@
+from random import randint
+from unittest import TestCase
+
+import hashlib
+import hmac
+
+
+class FieldElement:
+
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = 'Num {} not in field range 0 to {}'.format(
+                num, prime - 1)
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self):
+        return 'FieldElement_{}({})'.format(self.prime, self.num)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        return not (self == other)
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different Fields')
+        num = (self.num + other.num) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot subtract two numbers in different Fields')
+
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __mul__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot multiply two numbers in different Fields')
+        num = (self.num * other.num) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different Fields')
+        
+        return self.__class__(num, self.prime)
+
+    def __rmul__(self, coefficient):
+        num = (self.num * coefficient) % self.prime
+        return self.__class__(num=num, prime=self.prime)
+
+# tag::source1[]
+class Point:
+
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        if self.x is None and self.y is None:
+            return
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+    # end::source1[]
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
+
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        return not (self == other)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        elif isinstance(self.x, FieldElement):
+            return 'Point({},{})_{}_{} FieldElement({})'.format(
+                self.x.num, self.y.num, self.a.num, self.b.num, self.x.prime)
+        else:
+            return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
+        # Case 0.0: self is the point at infinity, return other
+        if self.x is None:
+            return other
+        # Case 0.1: other is the point at infinity, return self
+        if other.x is None:
+            return self
+
+
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x = s**2 - 2 * self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+    # tag::source3[]
+    def __rmul__(self, coefficient):
+        coef = coefficient
+        current = self  # <1>
+        result = self.__class__(None, None, self.a, self.b)  # <2>
+        while coef:
+            if coef & 1:  # <3>
+                result += current
+            current += current  # <4>
+            coef >>= 1  # <5>
+        return result
+    # end::source3[]
